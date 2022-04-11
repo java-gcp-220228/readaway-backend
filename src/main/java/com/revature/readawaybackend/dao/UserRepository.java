89649d226436c6040ae4cbde3bbc0bcb11818862
cerafinn <74612014+cerafinn@users.noreply.github.com>
2022-04-11 15:56:21 -0400

    created login and user DTO

--- src/main/java/com/revature/readawaybackend/dao/UserRepository.java
+++ src/main/java/com/revature/readawaybackend/dao/UserRepository.java
@@ -4,4 +4,5 @@ import com.revature.readawaybackend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+  public abstract User findByUsernameAndPassword(String username, String password);
 }
