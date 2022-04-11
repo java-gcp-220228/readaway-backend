0bf73d2b88df17a1984ae2c49be9404ca1e66fe8
JodhB <jodh_sb@hotmail.com>
2022-04-11 13:41:29 -0700

    Added tests for giveaway

--- src/main/java/com/revature/readawaybackend/dao/GiveawayRepository.java
+++ src/main/java/com/revature/readawaybackend/dao/GiveawayRepository.java
@@ -2,6 +2,9 @@ package com.revature.readawaybackend.dao;
 
 import com.revature.readawaybackend.models.Giveaway;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface GiveawayRepository extends JpaRepository<Giveaway, Integer> {
 }
