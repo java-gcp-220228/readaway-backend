38625e034eefd274c8c990e1568c5922af283ee9
cerafinn <andaleeb.farooq@gmail.com>
2022-04-13 13:29:24 -0400

    Revert "worked on controller: renamed file and added register postmapping, cr…"

--- src/main/java/com/revature/readawaybackend/controllers/AuthController.java
+++ src/main/java/com/revature/readawaybackend/controllers/AuthController.java
@@ -1,27 +1,26 @@
 package com.revature.readawaybackend.controllers;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.revature.readawaybackend.exceptions.UserExistsException;
 import com.revature.readawaybackend.models.LoginDTO;
 import com.revature.readawaybackend.models.User;
 import com.revature.readawaybackend.models.UserDTO;
 import com.revature.readawaybackend.service.AuthService;
 import com.revature.readawaybackend.service.JwtService;
-import com.revature.readawaybackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.security.auth.login.FailedLoginException;
 
 @RestController
 @CrossOrigin(originPatterns = "*", exposedHeaders = "*", allowedHeaders = "*")
-public class UserController {
+public class AuthController {
   @Autowired
   AuthService authService;
-  @Autowired
-  UserService userService;
   @Autowired
   JwtService jwtService;
 
@@ -40,18 +39,19 @@ public class UserController {
     }
   }
 
+
   @PostMapping("/register")
-  public ResponseEntity<?> register(@RequestBody UserDTO dto) throws JsonProcessingException {
-    try {
-      User registered = userService.register(dto);
-      String jwt = jwtService.createJwt(registered);
+  public ResponseEntity<?> register(@RequestBody User form) throws JsonProcessingException {
+      User user = authService.register(form.getEmail(), form.getUsername(), form.getPassword());
+      // set id for user is automatic?
+
+      String jwt = jwtService.createJwt(user);
 
       HttpHeaders responseHeaders = new HttpHeaders();
       responseHeaders.set("token", jwt);
 
-      return ResponseEntity.ok().headers(responseHeaders).body(registered);
-    } catch (UserExistsException e) {
-      return ResponseEntity.status(409).body(e.getMessage());
-    }
+      return ResponseEntity.ok().headers(responseHeaders).body(user);
+
+      // set catch blocks for existing username and existing email
   }
 }
--- src/main/java/com/revature/readawaybackend/models/UserDTO.java
+++ src/main/java/com/revature/readawaybackend/models/UserDTO.java
@@ -2,19 +2,10 @@ package com.revature.readawaybackend.models;
 
 import lombok.*;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 @NoArgsConstructor
 @Data
 public class UserDTO {
-  @NotNull
-  @Size(min = 1)
+  private int id;
   private String username;
-  @NotNull
-  @Size(min = 1)
-  private String password;
-  @NotNull
-  @Size(min = 1)
   private String email;
 }
