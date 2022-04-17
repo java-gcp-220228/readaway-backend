5fc4b402ea6c792e4b06f28a44bc7711a7553f29
cerafinn <74612014+cerafinn@users.noreply.github.com>
2022-04-17 13:46:10 -0400

    testing hashing

--- src/main/java/com/revature/readawaybackend/service/UserService.java
+++ src/main/java/com/revature/readawaybackend/service/UserService.java
@@ -3,6 +3,7 @@ package com.revature.readawaybackend.service;
 import com.revature.readawaybackend.dao.UserRepository;
 import com.revature.readawaybackend.exceptions.UserExistsException;
 import com.revature.readawaybackend.models.*;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,11 +28,14 @@ public class UserService {
       throw new UserExistsException("An account exists with username " + dto.getUsername());
     }
 
+    String plainPswd = dto.getPassword();
+    String hashed = BCrypt.hashpw(plainPswd, BCrypt.gensalt());
+
     User user = new User();
 
     user.setEmail(dto.getEmail());
     user.setUsername(dto.getUsername());
-    user.setPassword(dto.getPassword());
+    user.setPassword(hashed);
     // save or saveAndFlush?
     userRepo.saveAndFlush(user);
 
